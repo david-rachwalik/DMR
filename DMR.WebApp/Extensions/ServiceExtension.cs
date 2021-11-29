a26c28b06e60b661be4c9b91a5ed04da5980a18b
David Rachwalik <david.rachwalik@outlook.com>
2021-11-29 00:00:46 -0600

    extensions update

--- DMR.WebApp/Extensions/ServiceExtension.cs
+++ DMR.WebApp/Extensions/ServiceExtension.cs
@@ -3,34 +3,27 @@ using Microsoft.AspNetCore.Authentication;
 //using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-//using DMR.WebApp.Areas.Game.Services;
-//using DMR.WebApp.Areas.Game.Services.CharacterBodyParts;
 
-namespace DMR.WebApp.Extensions
+namespace DMR.WebApp.Extensions;
+
+public static class ServiceExtension
 {
-    public static class ServiceExtension
+    public static void AddDataServices(this IServiceCollection services)
     {
-        public static void AddDataServices(this IServiceCollection services)
-        {
-            // -------- Register Game Core Services --------
-            services.AddScoped<IAttributeService, AttributeService>();
-            services.AddScoped<ITagService, TagService>();
-            // --------------------------------------------------------
-            services.AddScoped<ICharacterTemplateService, CharacterTemplateService>();
-            services.AddScoped<IItemTemplateService, ItemTemplateService>();
-            services.AddScoped<ILocationTemplateService, LocationTemplateService>();
-            //// --------------------------------------------------------
-            services.AddScoped<IUserProfileService, UserProfileService>();
+        // -------- Register Game Core Services --------
+        services.AddScoped<IAttributeService, AttributeService>();
+        services.AddScoped<ITagService, TagService>();
+        // --------------------------------------------------------
+        services.AddScoped<ICharacterTemplateService, CharacterTemplateService>();
+        services.AddScoped<IItemTemplateService, ItemTemplateService>();
+        services.AddScoped<ILocationTemplateService, LocationTemplateService>();
+        //// --------------------------------------------------------
+        services.AddScoped<IUserProfileService, UserProfileService>();
 
 
-            //services.AddScoped<ICharacterBodyPartService, CharacterBodyPartService>();
-            //// --------------------------------------------------------
-            ////services.AddScoped<IAuditService, AuditService>();
-            //services.AddScoped<ISaveStateService, SaveStateService>();
-        }
+        //services.AddScoped<ICharacterBodyPartService, CharacterBodyPartService>();
+        //// --------------------------------------------------------
+        ////services.AddScoped<IAuditService, AuditService>();
+        //services.AddScoped<ISaveStateService, SaveStateService>();
     }
 }
