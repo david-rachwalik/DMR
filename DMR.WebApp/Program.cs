a3e2e7a5dc28e4b4fe02d1bec479c8d9f1a0dc8f
David Rachwalik <david.rachwalik@outlook.com>
2021-11-28 23:59:43 -0600

    upgrade project to net6.0

--- DMR.WebApp/Program.cs
+++ DMR.WebApp/Program.cs
@@ -1,50 +1,66 @@
 using DMR.WebApp.Data;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
 using DMR.WebApp.Extensions;
+using DMR.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace DMR.WebApp
+// -------- Configure Middleware Services --------
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationContext")));
+
+builder.Services.AddDataServices();
+
+// https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.dependencyinjection.databasedeveloperpageexceptionfilterserviceextensions.adddatabasedeveloperpageexceptionfilter?view=aspnetcore-6.0
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+// -------- Configure Application --------
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    // https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.builder.migrationsendpointextensions.usemigrationsendpoint?view=aspnetcore-6.0
+    app.UseMigrationsEndPoint();
+}
+
+// Seed the database when needed
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
     {
-        public static void Main(string[] args)
-        {
-            //var host = CreateHostBuilder(args).Build();
-
-            //CreateDbIfNotExists(host);
-
-            //host.Run();
-
-            CreateHostBuilder(args).Build().MigrateDatabase().Run();
-        }
-
-        // Old method -- archive when satisfied
-        private static void CreateDbIfNotExists(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred initializing the database.");
-                }
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        var context = services.GetRequiredService<ApplicationContext>();
+        DbInitializer.Initialize(context); // Migrate the database
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred initializing the database.");
     }
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
--- DMR.WebApp/README.md
+++ DMR.WebApp/README.md
@@ -4,11 +4,11 @@ A site to play around with web development, game design, and blogging about code
 
 
 ## Current Technologies Used:
-- <a href="https://docs.microsoft.com/en-us/dotnet/core/">ASP.NET Core</a>
- 	(<a href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/">C#</a>)
+- <a href="https://docs.microsoft.com/en-us/dotnet/core">ASP.NET 6.0</a>
+ 	(<a href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference">C#</a>)
 - Azure (<a href="https://docs.microsoft.com/en-us/azure/devops/boards">Planning</a>,
 	<a href="https://docs.microsoft.com/en-us/azure/devops/pipelines">CI</a>/<a href="https://docs.microsoft.com/en-us/azure/azure-resource-manager/">CD</a>,
 	<a href="https://docs.microsoft.com/en-us/azure/devops/repos/git">Git</a>,
 	<a href="https://docs.microsoft.com/en-us/azure/app-service/overview">WebApp</a>)
-- Visual Studio (<a href="https://docs.microsoft.com/en-us/visualstudio/ide/?view=vs-2019">2019</a>,
+- Visual Studio (<a href="https://docs.microsoft.com/en-us/visualstudio/ide/?view=vs-2019">2022</a>,
 	<a href="https://code.visualstudio.com/docs">Code</a>)
